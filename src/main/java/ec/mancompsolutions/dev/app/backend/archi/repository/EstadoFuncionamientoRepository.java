8637d7096f635b4b0d23a4e6b0660824e8de8d1e
desktop-jlp <jlparra@gmail.com>
2022-02-06 21:09:39 -0500

    Se agrego la anotacion @Repository en las interfaces de estados

--- src/main/java/ec/mancompsolutions/dev/app/backend/archi/repository/EstadoFuncionamientoRepository.java
+++ src/main/java/ec/mancompsolutions/dev/app/backend/archi/repository/EstadoFuncionamientoRepository.java
@@ -2,6 +2,8 @@ package ec.mancompsolutions.dev.app.backend.archi.repository;
 
 
 import ec.mancompsolutions.dev.app.backend.archi.entity.EstadoFuncionamiento;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EstadoFuncionamientoRepository extends BaseRepository <EstadoFuncionamiento,Long> {
 }
--- src/main/java/ec/mancompsolutions/dev/app/backend/archi/repository/EstadoInventarioRepository.java
+++ src/main/java/ec/mancompsolutions/dev/app/backend/archi/repository/EstadoInventarioRepository.java
@@ -2,6 +2,8 @@ package ec.mancompsolutions.dev.app.backend.archi.repository;
 
 import ec.mancompsolutions.dev.app.backend.archi.entity.EstadoInventario;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EstadoInventarioRepository extends BaseRepository<EstadoInventario,Long> {
 }
