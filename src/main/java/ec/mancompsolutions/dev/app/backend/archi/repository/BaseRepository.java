86b56ea5cc08d71bc185efb1e8a41bb0b397772f
desktop-jlp <jlparra@gmail.com>
2022-02-06 21:01:37 -0500

    Se agrego una interface generica BaseRepository para colocar los metodos que seran extendidos en las otras clases repository

---
+++ src/main/java/ec/mancompsolutions/dev/app/backend/archi/repository/BaseRepository.java
@@ -0,0 +1,11 @@
+package ec.mancompsolutions.dev.app.backend.archi.repository;
+
+import ec.mancompsolutions.dev.app.backend.archi.entity.BaseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+
+@NoRepositoryBean /* permite que de esta interface no se creen instancias */
+public interface BaseRepository <E extends BaseEntity, ID extends Serializable> extends JpaRepository<E,ID> {
+}
